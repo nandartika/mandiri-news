600384f701ad9c8081675dd8873496a656684241
cst_dev170 <cst_dev170@unitedtractors.com>
2023-07-25 20:05:21 +0700

    add(categoryData): Add list of category

---
+++ app/src/main/java/com/example/mandirinews/data/model/CategoryData.kt
@@ -0,0 +1,6 @@
+package com.example.mandirinews.data.model
+
+object CategoryData {
+	val categoryList =
+		listOf("business", "entertainment", "general", "health", "science", "sports", "technology")
+}
